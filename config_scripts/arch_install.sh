4310c4a978b6fd3b46c1a3414fdf8f19ce7a9fc0
Stevie Alvarez <stevie.6strings@gmail.com>
2023-05-24 18:43:35 -0400

    change login shell for client

--- config_scripts/arch_install.sh
+++ config_scripts/arch_install.sh
@@ -5,12 +5,13 @@ if [[ $# -lt 0 ]]; then
   echo "Usage: sh arch_install.sh <ADMIN_USERNAME>"
   exit 1
 else
+  pwd
   pacman -Syu
   echo ">>>INSTALLING zsh"
   pacman -S zsh
-  su -c chsh -s $(which zsh) $1
-  echo "testing"
-  sudo ./config_scripts/arch_fesh_install.sh "$1"
+  shell_path=$(which zsh)
+  usermod --shell $(which zsh) "$1"
+  sudo ./config_scripts/arch_fresh_install.sh "$1"
   #su -c ./config_scripts/arch_fresh_install.sh $1
   exit 0
 fi
