9547506739bf3f26c66e8bb0bd06659a40f61f53
Stevie Alvarez <stevie.6strings@gmail.com>
2023-09-10 02:42:36 -0400

    git: set up within install scripts

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # ensure admin username provided
 echo $#
 if [[ $# -lt 1 ]]; then
-  echo "Usage: sh install.sh [ADMIN_USERNAME] <Optional: github email, first name, last name (not required for github config)>"
+  echo "Usage: sh install.sh [ADMIN_USERNAME]"
   exit 1
 fi
 
@@ -19,27 +19,6 @@ else  # unsupported distro
   echo $distro
   echo ">>>UNSUPPORTED DISTRO TYPE"
   exit 1
-fi
-
-
-if [[ $# -eq 2 ]]; then
-  echo "Only provided github email, first name not included"
-  echo "Usage: sh install.sh [ADMIN_USERNAME] <Optional: github email, first name, last name (not required for github config)>"
-  exit 0
-
-else if [[ $# -eq 3 ]]; then
-  sh ./config_scripts/git_cred_setup.sh "$2" "$3"
-  exit 0
-
-else if [[ $# -eq 4 ]]; then
-  name="${3} ${4}"
-  sh ./config_scripts/git_cred_setup.sh "$2" "$name"
-  exit 0
-
-else
-  echo "Usage: sh install.sh [ADMIN_USERNAME] <Optional: github email, first name, last name (not required for github config)>"
-  exit 1
-
 fi
 
 exit 0
