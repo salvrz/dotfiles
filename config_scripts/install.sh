f5607ffb7e5589326b184135553176f0353bab2a
Stevie Alvarez <stevie.6strings@gmail.com>
2024-09-06 17:03:24 -0700

    install: formatted and styled

--- config_scripts/install.sh
+++ config_scripts/install.sh
@@ -1,16 +1,31 @@
 #! /bin/zsh
 
-# setup
-echo ">>>SETUP..."
-cwd=$(pwd)
-config="${cwd}/.config"
-client_home="/home/${1}"
+# SETUP {{{
 
-echo ">>>INSTALLING direnv"
-sudo pacman -S direnv
+    echo ">>>SETUP..."
+    cwd=$(pwd)
+    config="${cwd}/.config"
+    client_home="/home/${1}"
 
-echo ">>>COPYING DOTFILES"
-echo "\t...copying ${1}'s home directory"
-cp -r $cwd/home/* $client_home
-echo "\t...copying root"
-sudo cp -r $cwd/root /
+# }}}
+
+
+# DIRENV {{{
+
+    echo ">>>INSTALLING direnv"
+    sudo pacman -S direnv
+
+# }}}
+
+
+# DOTFILES {{{
+
+    echo ">>>COPYING DOTFILES"
+
+    echo "\t...copying ${1}'s home dir"
+    cp -r $cwd/home/* $client_home
+
+    echo "\t...copying root"
+    sudo cp -r $cwd/root /
+
+# }}}
