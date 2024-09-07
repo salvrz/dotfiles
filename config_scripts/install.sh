4c6e513a20c21abd89567d3422bbb2a41b244a5d
Stevie Alvarez <stevie.6strings@gmail.com>
2024-09-06 17:34:58 -0700

    install some more things like languages

--- config_scripts/install.sh
+++ config_scripts/install.sh
@@ -9,11 +9,64 @@
 
 # }}}
 
+# PARU {{{
+
+    echo ">>>INSTALLING paru (ASSUMING YAY IS INSTALLED)"
+    yay -S paru
+
+# }}}
+
 
 # DIRENV {{{
 
     echo ">>>INSTALLING direnv"
     sudo pacman -S direnv
+
+# }}}
+
+
+# AUDIO {{{
+
+    echo ">>>INSTALLING wireplumber"
+    paru -S wireplumber
+
+# }}}
+
+
+# LANGUAGES {{{
+
+    echo ">>>INSTALLING languages & package managers"
+
+    # NODEJS & NPM {{{
+
+        echo "\t...nodejs & npm"
+        sudo pacman -S nodejs
+        sudo pacman -S npm
+
+    # }}}
+
+
+    # RUST {{{
+
+        echo "\t...rust"
+        sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+        # TODO this doesn't quite work, 
+
+        echo ">>>USER INTERACTION REQUIRED"
+        echo"!!!finish installing rust!!!"
+        echo"!!!finish installing rust-analyzer!!!"
+        echo"!!!update this install code!!!"
+        read -n 1 -s  # wait for user input
+        #rustup install stable
+        #rustup component add rust-src
+        #source "$client_home/.cargo/env"
+        #tset
+        #git clone https://github.com/rust-lang/rust-analyzer.git
+        #cd rust-analyzer
+        #cargo xtask install
+
+    # }}}
 
 # }}}
 
