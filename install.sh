8e99a64ca2a5af4e60a4f0b38dbcb142d4912d15
Stevie Alvarez <stevie.6strings@gmail.com>
2024-09-09 16:26:37 -0700

    install: main install script calls correct helper script

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ distro=$(grep "ID_LIKE=" /etc/os-release)
 
 if echo $distro | grep arch; then ## arch based distros
   echo ">>>LOGGING TO out.txt"
-  ./config_scripts/arch_install.sh "$1" | tee -a out.txt
+  ./config_scripts/install.sh "$1" | tee -a out.txt
 
 else  # unsupported distro
   echo $distro
